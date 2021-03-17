e20af5321f375700793ed4ad5d003383ce1d864d
Tim Quimbo <tquimbo1@gmail.com>
2021-03-16 22:53:36 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+require "pry"
+
+class Artist
+
+    attr_accessor :name, :song, :genre
+    
+    @@all = []
+    
+    def initialize(name)
+    @name = name
+    @@all << self
+    end
+    
+    def self.all
+    @@all
+    end
+
+    def songs
+    Song.all.select {|song| song.artist == self}
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+    
+    def genres
+        songs.map {|song| song.genre}
+    end
+    
+end
